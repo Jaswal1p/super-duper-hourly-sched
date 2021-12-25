53a22efa1401a1ca046655ca27cce20645241154
Jaswal1p <Jaswal1p@yahoo.com>
2021-12-25 03:14:29 -0500

    worked on js code for click capture of specific button in html

---
+++ assets/addscript.js
@@ -0,0 +1,49 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+$(document).ready(function() {
+    $('#name').keyup(function() {
+        $(this).attr('size', $(this).val().length)
+    });
+});
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
+
+
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
--- script.js
+++ script.js
@@ -1,46 +1,6 @@
-$(document).ready(function() {
-    $('#name').keyup(function() {
-        $(this).attr('size', $(this).val().length)
-    });
+var eightBtn = document.querySelector("#eight-task");
+
+
+eightBtn.addEventListener("click", function() {
+  alert("button clicked");
 });
-
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
-
-
-
-
-const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
-
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
