f95e1bdef4b7059de778c8355aefa1c17eb67a5b
Jaswal1p <Jaswal1p@yahoo.com>
2021-12-25 21:21:18 -0500

    added live time plus individual button id with captured indv clicks

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-var eightBtn = document.querySelector("#eight-task");
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime; 
+
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
 
 
-eightBtn.addEventListener("click", function() {
-  alert("button clicked");
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
+    }
+  }
 });
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
